37196d04b0eb731113d3aec27e32c786f9329f34
Tinkidinki <mahathi98@gmail.com>
2017-09-16 00:00:28 +0530

    login tested

--- quiz_application/app/controllers/sessions_controller.rb
+++ quiz_application/app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user.try(:authenticate, params[:password])
       session[:user_id] = user.id 
-      redirect_to quiz_url
+      session[:user_name] = user.name
+      redirect_to "/quizzes"
     else
       redirect_to login_url, alert: "Invalid user/password combination"
     end
@@ -17,5 +18,5 @@ class SessionsController < ApplicationController
     session[:user_name] = nil
     redirect_to root, notice: "Logged out"
   end
-  
+
 end
